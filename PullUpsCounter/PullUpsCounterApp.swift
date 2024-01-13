b305ad4721e8bd37a26c63c1569e3119e6596b36
Alexander Riakhin <bonney977@gmail.com>
2024-01-13 15:18:24 +0100

    Initial Commit

---
+++ PullUpsCounter/PullUpsCounterApp.swift
@@ -0,0 +1,20 @@
+//
+//  PullUpsCounterApp.swift
+//  PullUpsCounter
+//
+//  Created by Aleksandr Riakhin on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PullUpsCounterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
