4149bb39c3edf339cd39e5a898f5e6ee95ff85a7
Alexander Ryakhin <bonney977@gmail.com>
2025-02-02 03:53:28 +0100

    Removed custom calendar, up version to iOS 17 because of some features I need to add

--- RepsCount/UI/CalendarFlow/CalendarScreen/CalendarScreen.swift
+++ RepsCount/UI/CalendarFlow/CalendarScreen/CalendarScreen.swift
@@ -6,60 +6,13 @@
 //
 
 import SwiftUI
-import MijickCalendarView
-import Combine
 import CoreData
 import Flow
-
-final class CalendarScreenViewModel: ObservableObject {
-    @AppStorage("savesLocation") var savesLocation: Bool = true
-    @Published var events: [CalendarEvent] = []
-
-    private let calendarEventStorage: CalendarEventStorageInterface
-    private let exerciseStorage: ExerciseStorageInterface
-    private var cancellable: Set<AnyCancellable> = []
-
-    init(
-        calendarEventStorage: CalendarEventStorageInterface,
-        exerciseStorage: ExerciseStorageInterface
-    ) {
-        self.calendarEventStorage = calendarEventStorage
-        self.exerciseStorage = exerciseStorage
-        setupBindings()
-    }
-
-    func remove(_ event: CalendarEvent) {
-        do {
-            try calendarEventStorage.deleteEvent(event)
-        } catch {
-            print(error)
-        }
-    }
-
-    func addExercisesToTheList(_ exercises: [ExerciseModel]) {
-        exercises.forEach { model in
-            exerciseStorage.addExerciseFromExerciseModel(model, savesLocation: savesLocation)
-        }
-    }
-
-    private func setupBindings() {
-        calendarEventStorage.eventsPublisher
-            .sink { completion in
-                // TODO: error handle
-            } receiveValue: { [weak self] events in
-                self?.events = events
-            }
-            .store(in: &cancellable)
-    }
-}
 
 struct CalendarScreen: View {
     private let resolver = DIContainer.shared.resolver
 
-    @State private var selectedDate: Date? = .now
-    @State private var selectedMonth: Date = .now
-    @State private var selectedRange: MDateRange? = .init()
-
+    @State private var selectedDate: Date = .now
     @ObservedObject private var viewModel: CalendarScreenViewModel
 
     private var groupedEvents: [Date: [CalendarEvent]] {
@@ -77,21 +30,15 @@ struct CalendarScreen: View {
         NavigationView {
             ScrollView {
                 VStack {
-                    MCalendarView(
-                        selectedDate: $selectedDate,
-                        selectedRange: $selectedRange
-                    ) { calendar in
-                        calendar
-                            .firstWeekday(.monday)
-                            .dayView(Price.init)
-                            .startMonth(selectedMonth)
-                            .endMonth(selectedMonth)
-                            .monthLabel(MyMonthLabel.init)
-                    }
-                    .scrollDisabled(true)
-                    .fixedSize(horizontal: false, vertical: true)
+                    DatePicker(
+                        "Select Date",
+                        selection: $selectedDate,
+                        displayedComponents: [.date]
+                    )
+                    .datePickerStyle(.graphical)
+                    .clippedWithBackground()
 
-                    if let selectedDate, let events = groupedEvents[selectedDate.trimmed()] {
+                    if let events = groupedEvents[selectedDate.trimmed()] {
                         ForEach(events) { event in
                             workoutCard(for: event)
                         }
@@ -109,10 +56,9 @@ struct CalendarScreen: View {
                         .clippedWithBackground()
                     }
                 }
-                .padding(.horizontal, 16)
+                .padding(16)
             }
             .navigationTitle("Calendar")
-            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     NavigationLink("Add event") {
@@ -155,7 +101,7 @@ struct CalendarScreen: View {
                         }
                     } label: {
                         Image(systemName: "ellipsis.circle.fill")
-                            .font(.title)
+                            .font(.headline)
                     }
                     .foregroundStyle(.secondary)
                 }
@@ -193,75 +139,6 @@ struct CalendarScreen: View {
             .frame(maxWidth: .infinity, alignment: .leading)
             .clippedWithBackground()
         }
-    }
-}
-
-struct Price: DayView {
-    let date: Date
-    let isCurrentMonth: Bool
-    let selectedDate: Binding<Date?>?
-    let selectedRange: Binding<MDateRange?>?
-
-    var isSelected: Bool {
-        date.trimmed() == selectedDate?.wrappedValue?.trimmed()
-    }
-
-    func createContent() -> AnyView {
-        VStack(spacing: 4) {
-            Text(getStringFromDay(format: "d"))
-                .font(.headline)
-                .foregroundStyle(.primary)
-
-            dotsGridView
-        }
-        .opacity(isPast() ? 0.5 : 1)
-        .frame(maxWidth: .infinity)
-        .padding(.vertical, 8)
-        .background(Color.surface)
-        .overlay {
-            (isSelected ? Color.accentColor : .clear)
-                .clipShape(
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(style: .init(lineWidth: 4))
-                )
-        }
-        .clipShape(RoundedRectangle(cornerRadius: 16))
-        .padding(2)
-        .erased()
-    }
-
-    func onSelection() {
-        if !isPast() {
-            selectedDate?.wrappedValue = date
-        }
-    }
-
-    private var dotsGridView: some View {
-        LazyHGrid(rows: Array(repeating: GridItem(.flexible(minimum: 4, maximum: 4)), count: 2), spacing: 4) {
-            ForEach(0..<Int.random(in: 1...8)) { _ in
-                Circle()
-                    .fill(Color.red)
-                    .frame(width: 5, height: 5)
-            }
-        }
-        .padding(.horizontal, 8)
-    }
-}
-
-struct MyMonthLabel: MonthLabel {
-    var month: Date
-
-    func createContent() -> AnyView {
-        Text(month.formatted(
-            Date.FormatStyle()
-                .year(.defaultDigits)
-                .month(.wide)
-        ))
-        .font(.system(.title, design: .default, weight: .bold))
-        .foregroundStyle(.primary)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.horizontal, 16)
-        .erased()
     }
 }
 
